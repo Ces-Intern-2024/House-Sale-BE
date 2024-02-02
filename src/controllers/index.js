2c64cbf95c241b5132e8214144904023041f6075
NhatTrinhLe <nhattrinh.le.it@gmail.com>
2024-02-02 14:51:58 +0700

    feat: build API get all provinces, get all districts by provinceCode, get all wards by districtCode

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,3 @@
 module.exports.propertyController = require('./property.controller')
 module.exports.userController = require('./user.controller')
+module.exports.locationController = require('./location.controller')
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,3 @@
 module.exports.userValidation = require('./user.validation')
 module.exports.propertyValidation = require('./property.validation')
+module.exports.locationValidation = require('./location.validation')
